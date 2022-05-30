d4d3a0916d9244c51a5a6eb89311ea81ab1b53f6
KhanhHuynh <khanh0787722636@gmail.com>
2022-05-30 09:21:34 +0700

    Remove Whitelist button

---
+++ src/main/java/me/khanh/plugins/extrastorageform/forms/StorageForm.java
@@ -0,0 +1,8 @@
+package me.khanh.plugins.extrastorageform.forms;
+
+import dev.dejvokep.boostedyaml.block.implementation.Section;
+
+public class StorageForm {
+    public StorageForm(Section section){
+    }
+}
